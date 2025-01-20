7d502e01bcfcd3a623f5b2be8e23f179a6223fda
btcbarry <btcbarry1@gmail.com>
2025-01-20 19:48:50 +0000

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    API_ENDPOINT: '/api/chat',  // Will handle API requests through our server
-    MAX_CONTEXT_LENGTH: 10,     // Maximum number of messages to keep in context
-    TYPING_DELAY: 500,         // Delay before showing typing indicator
-    SUGGESTIONS_COUNT: 3       // Number of suggestions to show
-}; 
